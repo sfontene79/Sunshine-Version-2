b89b79f4a2b7f93f62dc444d1945dd1c47e600a5
Sylvain Fonteneau <sylvain.fonteneau@thalesgroup.com>
2015-06-10 17:26:21 +0200

    Lesson 3: Add a share button.

--- app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.android.sunshine.app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,7 +19,18 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private ShareActionProvider mShareProvider;
+    private String mWeatherText;
+
     public DetailActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Announce that this fragment has menu items
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -23,11 +38,36 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-        String weatherText = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        mWeatherText = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
 
         TextView textView = (TextView)fragmentView.findViewById(R.id.debug_text);
-        textView.setText(weatherText);
+        textView.setText(mWeatherText);
         return fragmentView;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Complete menu with detail items
+        inflater.inflate(R.menu.menu_detail_fragment, menu);
+
+        // Get share provider
+        MenuItem item = menu.findItem(R.id. action_share);
+        mShareProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        // Update share provider
+        setShareIntent();
+    }
+
+    private void setShareIntent() {
+        // Shared text
+        String shareText = mWeatherText + " #SUNSHINE";
+
+        // Intent used for sharing
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+        shareIntent.setType("text/plain");
+        if (mShareProvider != null) {
+            mShareProvider.setShareIntent(shareIntent);
+        }
+    }
 }
